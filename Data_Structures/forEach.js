a6a3436369bb19999aae7a09611c9e9f59466e6e
github username <github email address>
2020-07-12 22:14:26 -0500

    ForEach are hard to understand

--- Data_Structures/forEach.js
+++ Data_Structures/forEach.js
@@ -1,6 +1,25 @@
+//first draft
+
 var evens = [0,2,4,6,8,10];
 
 for (var i = 0; index < evens.length; i++) {
     console.log(`${evens[i]} is not odd!`);
     
 }
+
+//second draft
+
+var evens = [0,2,4,6,8,10];
+
+evens.forEach(even => {
+    console.log(`${even} is not odd!`);
+});
+
+// with debugger
+
+var evens = [0,2,4,6,8,10];
+
+evens.forEach((even, index, array) => {
+    debugger;
+    console.log(`${even} is not odd!`);
+});
