eddd8c33ad3e47147050b32ad649d46720961802
github username <github email address>
2020-07-25 22:04:19 -0500

    Switch

---
+++ switch.js
@@ -0,0 +1,18 @@
+function caseInSwitch(val) {
+    var answer = "";
+    // Only change code below this line
+    switch(val){
+      case 1:
+        return "alpha"
+        break;
+      case 2:
+        return "beta"
+        break;
+      case 3:
+        return "gamma"
+        break;
+      case 4:
+        return "delta"
+        break;
+      
+    }
