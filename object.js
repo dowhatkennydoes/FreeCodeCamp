871de480aef83816aa3af05fe92b9ecc2a57765f
github username <github email address>
2020-07-27 21:47:11 -0500

    Another object exercise

---
+++ object.js
@@ -0,0 +1,10 @@
+var myDog = {
+    // Only change code below this line
+      "name": "logan",
+      "legs": 9,
+      "tails": 0,
+      "friends": ["hannah", "kenny", "Tommy"]
+    
+    // Only change code above this line
+    };
+    
